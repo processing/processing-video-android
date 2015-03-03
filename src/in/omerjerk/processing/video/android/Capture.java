a4080d3ca9ae8c64eabaa8cc97f64b82c9a8164f
Umair Khan <omerjerk@gmail.com>
2015-03-03 20:54:00 +0530

    constructor overloading
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- src/in/omerjerk/processing/video/android/Capture.java
+++ src/in/omerjerk/processing/video/android/Capture.java
@@ -14,18 +14,29 @@ public class Capture extends PImage implements PConstants {
 	
 	private static Context context;
 	
-	private static ArrayList<String> camerasList = new ArrayList<>();
+	private static ArrayList<String> camerasList = new ArrayList<String>();
 	
 	private static final String KEY_FRONT_CAMERA = "front-camera-%d";
 	private static final String KEY_BACK_CAMERA = "back-camera-%d";
 	
+	private int selectedCamera = 0;
+	
 	public Capture (PApplet context) {
-		Capture.context = context;
+		this(context, null);
 	}
 	
+	public Capture (PApplet context, String camera) {
+		Capture.context = context;
+		if (camera == null || camera.equals("")) {
+			selectedCamera = 0;
+		} else {
+			selectedCamera = camerasList.indexOf(camera);
+		}
+	}
+
 	public static String[] list() {
 		ensureContext();
-		if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
+		if (true) {
 			int nOfCameras = Camera.getNumberOfCameras();
 			for (int i = 0; i < nOfCameras; ++i) {
 				Camera.CameraInfo cameraInfo = new Camera.CameraInfo();
@@ -46,7 +57,10 @@ public class Capture extends PImage implements PConstants {
 	
 	private static void ensureContext() {
 		if (context == null) {
-			throw new RuntimeException("Create the instance of Capture class first.");
+			context = PApplet.getInstance();
+			if (context == null) {
+				throw new RuntimeException("Create the instance of Capture class first.");
+			}			
 		}
 	}
 }
