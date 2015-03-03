37d556d5aab8a031bd2c9400a1bab62eb87a0d6e
Umair Khan <omerjerk@gmail.com>
2015-03-03 23:44:17 +0530

    Add Camera preview class
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- src/in/omerjerk/processing/video/android/Capture.java
+++ src/in/omerjerk/processing/video/android/Capture.java
@@ -1,18 +1,25 @@
 package in.omerjerk.processing.video.android;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
 
-@SuppressWarnings("deprecation")
+//@SuppressWarnings("deprecation")
 public class Capture extends PImage implements PConstants {
 	
-	private static Context context;
+	private static final boolean DEBUG = true;
+	public static void log(String log) {if (DEBUG) System.out.println(log);}
+	
+	private Context context;
 	
 	private static ArrayList<String> camerasList = new ArrayList<String>();
 	
@@ -22,20 +29,19 @@ public class Capture extends PImage implements PConstants {
 	private int selectedCamera = 0;
 	
 	public Capture (PApplet context) {
-		this(context, null);
+		this.context = context;
 	}
 	
-	public Capture (PApplet context, String camera) {
-		Capture.context = context;
+	public void setCamera(String camera) {
 		if (camera == null || camera.equals("")) {
 			selectedCamera = 0;
 		} else {
 			selectedCamera = camerasList.indexOf(camera);
 		}
+		log("Selected camera = " + selectedCamera);		
 	}
 
-	public static String[] list() {
-		ensureContext();
+	public String[] list() {
 		if (true) {
 			int nOfCameras = Camera.getNumberOfCameras();
 			for (int i = 0; i < nOfCameras; ++i) {
@@ -55,12 +61,73 @@ public class Capture extends PImage implements PConstants {
 		return null;
 	}
 	
-	private static void ensureContext() {
-		if (context == null) {
-			context = PApplet.getInstance();
-			if (context == null) {
-				throw new RuntimeException("Create the instance of Capture class first.");
-			}			
+	private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+		
+		@Override
+		public void onPreviewFrame(byte[] arg0, Camera arg1) {
+			log("Received Camera frame");
 		}
+	};
+	
+	private class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+		
+		private Camera mCamera;
+		private SurfaceHolder mHolder;
+		
+		public CameraPreview(Context context, Camera camera) {
+			super(context);
+			this.mCamera = camera;
+			
+			mHolder = getHolder();
+			mHolder.addCallback(this);
+			mHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+		}
+		
+		@Override
+	    public void surfaceCreated(SurfaceHolder holder) {
+			try {
+	            mCamera.setPreviewDisplay(holder);
+	            mCamera.startPreview();
+	        } catch (IOException e) {
+	            Log.d("PROCESSING", "Error setting camera preview: " + e.getMessage());
+	            e.printStackTrace();
+	        }
+	    }
+
+	    @Override
+	    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	    	// If your preview can change or rotate, take care of those events here.
+	        // Make sure to stop the preview before resizing or reformatting it.
+
+	        if (mHolder.getSurface() == null){
+	          // preview surface does not exist
+	          return;
+	        }
+
+	        // stop preview before making changes
+	        try {
+	            mCamera.stopPreview();
+	        } catch (Exception e){
+	          // ignore: tried to stop a non-existent preview
+	        }
+
+	        // set preview size and make any resize, rotate or
+	        // reformatting changes here
+
+	        // start preview with new settings
+	        try {
+	            mCamera.setPreviewDisplay(mHolder);
+	            mCamera.startPreview();
+
+	        } catch (Exception e){
+	            Log.d("PROCESSING", "Error starting camera preview: " + e.getMessage());
+	            e.printStackTrace();
+	        }
+	    }
+
+	    @Override
+	    public void surfaceDestroyed(SurfaceHolder holder) {
+	        // do nothing
+	    }
 	}
 }
