9d76d3d203129a23938074064847430024c8474c
Umair Khan <omerjerk@gmail.com>
2015-03-03 14:04:58 +0530

    Buildable with eclipse
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

---
+++ src/in/omerjerk/processing/video/android/Capture.java
@@ -0,0 +1,8 @@
+package in.omerjerk.processing.video.android;
+
+import processing.core.PConstants;
+import processing.core.PImage;
+
+public class Capture extends PImage implements PConstants {
+	
+}
---
+++ src/in/omerjerk/processing/video/android/Movie.java
@@ -0,0 +1,8 @@
+package in.omerjerk.processing.video.android;
+
+import processing.core.PConstants;
+import processing.core.PImage;
+
+public class Movie extends PImage implements PConstants {
+	
+}
