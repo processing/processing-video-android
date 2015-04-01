c6cc8adfcdc53aaa84d3492c583b794f6a14c2dc
Umair Khan <omerjerk@gmail.com>
2015-04-01 20:08:17 +0530

    expected constructors for Movie class
    Signed-off-by: Umair Khan <omerjerk@gmail.com>

--- src/in/omerjerk/processing/video/android/Movie.java
+++ src/in/omerjerk/processing/video/android/Movie.java
@@ -1,8 +1,20 @@
 package in.omerjerk.processing.video.android;
 
+import processing.core.PApplet;
 import processing.core.PConstants;
 import processing.core.PImage;
 
 public class Movie extends PImage implements PConstants {
 	
+	public Movie(PApplet parent) {
+		this(parent, -1, -1);
+	}
+	
+	public Movie(PApplet parent, int width, int height) {
+		if (width == -1 || height == -1) {
+			width = 720;
+			height = 1280;
+		}
+		init(width, height, ARGB);
+	}
 }
