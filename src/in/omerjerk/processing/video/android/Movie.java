4f6708fcd582a0c514ad62f4f34432181688f143
Kate Hollenbach <kjhollen@gmail.com>
2023-03-15 18:17:36 -0600

    Update build system to gradle
    use `gradle dist` to build project, which will also copy it into the
    user's processing sketchbook folder, and prepare a .zip file for
    online distribution.
    
    generates the library.properties file from a template, copying data
    over from build.properties (both found in the resources folder).
    
    uses new maven repository created by @codeanticode
    
    updates .mov -> .mp4 in example, for better Android compatibility.
    
    updated README with brief build instructions.
    will add Android Studio instructions later.

--- src/in/omerjerk/processing/video/android/Movie.java
+++ src/in/omerjerk/processing/video/android/Movie.java
@@ -16,131 +16,126 @@ import android.view.Surface;
 import processing.core.PApplet;
 
 public class Movie extends VideoBase implements MediaPlayerHandlerCallback {
-    
-    private MediaPlayerHandler handler;
-    private MediaPlayer player;
-    
-    private boolean looping = false;
-	
+
+	private MediaPlayerHandler handler;
+	private MediaPlayer player;
+
+	private boolean looping = false;
+
 	public Movie(PApplet parent, String fileName) {
-	    super(parent);
-	    AssetFileDescriptor afd = null;
-	    try {
-            afd = activity.getAssets().openFd(fileName);
-            MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
-            metaRetriever.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-            String height = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
-            String width = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
-            if (height == null || width == null) {
-            	showDialog("Error!", "This video format is not supported by the device."
-                    + "Please try a video with another format.");
-            	return;
-            }
-            init(Integer.valueOf(width), Integer.valueOf(height), ARGB);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-	    
-	    initalizeFrameBuffer();
-	    
-	    HandlerThread backgroundThread = new HandlerThread("MediaPlayer");
-	    backgroundThread.start();
-	    handler = new MediaPlayerHandler(backgroundThread.getLooper());
-	    handler.setCallback(this);
-	    handler.sendMessage(handler.obtainMessage(MediaPlayerHandler.MSG_INIT_PLAYER, afd));
+		super(parent);
+		AssetFileDescriptor afd = null;
+		try {
+			afd = activity.getAssets().openFd(fileName);
+			MediaMetadataRetriever metaRetriever = new MediaMetadataRetriever();
+			metaRetriever.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+			String height = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT);
+			String width = metaRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH);
+			if (height == null || width == null) {
+				showDialog("Error!", "This video format is not supported by the device."
+						+ "Please try a video with another format.");
+				return;
+			}
+			init(Integer.valueOf(width), Integer.valueOf(height), ARGB);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		initalizeFrameBuffer();
+
+		HandlerThread backgroundThread = new HandlerThread("MediaPlayer");
+		backgroundThread.start();
+		handler = new MediaPlayerHandler(backgroundThread.getLooper());
+		handler.setCallback(this);
+		handler.sendMessage(handler.obtainMessage(MediaPlayerHandler.MSG_INIT_PLAYER, afd));
 	}
-	
+
 	public void play() {
-	    handler.sendMessage(handler.obtainMessage(MediaPlayerHandler.MSG_START_PLAYER));
+		handler.sendMessage(handler.obtainMessage(MediaPlayerHandler.MSG_START_PLAYER));
 	}
-	
+
 	public void loop() {
-	    looping = true;
+		looping = true;
 	}
-	
+
 	@Override
 	public void onPause() {
 	}
-	
+
 	@Override
 	public void onResume() {
-	    initalizeFrameBuffer();
+		initalizeFrameBuffer();
 	}
-	
+
 	@Override
 	public String getEventName() {
-	    return "movieEvent";
+		return "movieEvent";
 	}
-	
-	private class MediaPlayerHandler extends Handler {
-	    
-	    public static final int MSG_INIT_PLAYER = 0;
-	    public static final int MSG_START_PLAYER = 1;
-	    
-	    MediaPlayerHandlerCallback callback;
-	    
-	    public MediaPlayerHandler(Looper looper) {
-            super(looper);
-        }
 
-        public void setCallback (MediaPlayerHandlerCallback cb) {
-	        this.callback = cb;
-	    }
-	    
-	    @Override
-	    public void handleMessage(Message msg) {
-	        switch (msg.what) {
-	        case MSG_INIT_PLAYER:
-	            AssetFileDescriptor afd = (AssetFileDescriptor) msg.obj;
-	            callback.initPlayer(afd);
-	            break;
-            case MSG_START_PLAYER:
-                callback.startPlayer();
-                break;
-            default:
-                break;
-            }
-	    }
+	private class MediaPlayerHandler extends Handler {
+
+		public static final int MSG_INIT_PLAYER = 0;
+		public static final int MSG_START_PLAYER = 1;
+
+		MediaPlayerHandlerCallback callback;
+
+		public MediaPlayerHandler(Looper looper) {
+			super(looper);
+		}
+
+		public void setCallback (MediaPlayerHandlerCallback cb) {
+			this.callback = cb;
+		}
+
+		@Override
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+				case MSG_INIT_PLAYER:
+					AssetFileDescriptor afd = (AssetFileDescriptor) msg.obj;
+					callback.initPlayer(afd);
+					break;
+				case MSG_START_PLAYER:
+					callback.startPlayer();
+					break;
+				default:
+					break;
+			}
+		}
 	}
-	
+
 	@Override
 	public void initPlayer(AssetFileDescriptor afd) {
-	    player = new MediaPlayer();
-	    try {
-            player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
-            System.out.println("texture id = " + mTextureId);
-            while (mTextureId == 0) {
-                Thread.sleep(100);
-            }
-            player.setSurface(new Surface(mSurfaceTexture));
-            player.setLooping(looping);
-            player.prepare();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-	}
-	
-	public void dispose() {
-		player.stop();
-		player.release();
+		player = new MediaPlayer();
+		try {
+			player.setDataSource(afd.getFileDescriptor(), afd.getStartOffset(), afd.getLength());
+			System.out.println("texture id = " + mTextureId);
+			while (mTextureId == 0) {
+				Thread.sleep(100);
+			}
+			player.setSurface(new Surface(mSurfaceTexture));
+			player.setLooping(looping);
+			player.prepare();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
 	public void startPlayer() {
-	    player.start();
+		player.start();
 	}
-	
+
 	private void showDialog(String title, String message) {
 		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(activity);
 		dialogBuilder.setMessage(message);
 		dialogBuilder.setMessage(title);
-		
+
 		dialogBuilder.setPositiveButton("Okay", new DialogInterface.OnClickListener() {
-	           public void onClick(DialogInterface dialog, int id) {
-	               activity.finish();
-	           }
-	    });
-		
+			public void onClick(DialogInterface dialog, int id) {
+				activity.finish();
+			}
+		});
+
 		AlertDialog dialog = dialogBuilder.create();
 		dialog.show();
 	}
